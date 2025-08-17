281f28bd5b00f207b4c5c2cebc61715912a94e06
CWmain <cameronwhite2478@gmail.com>
2025-08-17 19:33:34 +1000

    Have a basic replay going without weird tweening which is good. Just need to get the portal moving and the old portals moving

--- objects/board/board.gd
+++ objects/board/board.gd
@@ -7,14 +7,21 @@ var cellSize: int = 32
 # Number of cells in columns and rows (cellCount, cellCount)
 var cellCount: int = 20
 
+# The time between each tick
 @export var tickTime: float = 0.5
-
+var tickCount: int = 0
 @onready var tick: Timer = $Tick
+
+@export var chaserScene: PackedScene
 @onready var chaser: Chaser = $Chaser
+var chasers: Array[Chaser]
+
+var portalPos: Vector2 = Vector2(5,5)
 
 func _ready() -> void:
 	tick.wait_time = tickTime
 	tick.timeout.connect(_on_tick)
+	chasers.append(chaser)
 
 func gridToReal(gridPos: Vector2) -> Vector2:
 	var realPos: Vector2
@@ -22,9 +29,41 @@ func gridToReal(gridPos: Vector2) -> Vector2:
 	return realPos
 
 func _on_tick() -> void:
-	chaser.updateChaserPosition()
+	for c in chasers:
+		c.updateChaserPosition()
+	chaserOutOfBounds()
+	chaserOnPortal()
+	tickCount += 1
+
+func chaserOutOfBounds() -> void:
+	var pos: Vector2 = chasers[-1].position
+	var xCheck: bool = pos.x < 0 || pos.x >= cellCount
+	var yCheck: bool = pos.y < 0 || pos.y >= cellCount
+	if yCheck or xCheck:
+		endGame()
+
+func chaserOnPortal() -> void:
+	if chasers[-1].position == portalPos:
+		chasers[-1].unactivateChaser()
+		tickCount = 0
+		for c in chasers:
+			c.setChaserPositionToStart()
+		newChaser(Vector2(portalPos))
 
 # Returns the time forr 1 tick to complete
 func get_tick_time() -> float:
 	return tickTime
 	
+func newChaser(spawnLoc: Vector2) -> void:
+	chasers.append(chaserScene.instantiate())
+	add_child(chasers[-1])
+	chasers[-1].spawnChaser(spawnLoc)	
+
+func pauseTicks() -> void:
+	tick.paused = true
+	
+func resumeTicks() -> void:
+	tick.paused = false
+
+func endGame():
+	print("Game is over")
--- objects/chaser/chaser.gd
+++ objects/chaser/chaser.gd
@@ -7,14 +7,16 @@ class_name Chaser
 
 var board: Board
 
-var moveDirection: Vector2 = Vector2(0,1)
+var moveDirection: Vector2
 var position: Vector2
 
-var startingTick: int
+var startingTick: int = 0
 var previousPositions: Array[Vector2]
 
 var currentTween: Tween
 @export var tweenTime: float = 0.5
+
+var active: bool = true
 
 var up: Vector2 = Vector2(0, -1)
 var down: Vector2 = Vector2(0, 1)
@@ -27,17 +29,43 @@ func _ready() -> void:
 	tweenTime = board.get_tick_time()
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("moveUp"):
-		moveDirection = up
-	if Input.is_action_just_pressed("moveDown"):
-		moveDirection = down
-	if Input.is_action_just_pressed("moveLeft"):
-		moveDirection = left
-	if Input.is_action_just_pressed("moveRight"):
-		moveDirection = right
+	if active:
+		if Input.is_action_just_pressed("moveUp"):
+			moveDirection = up
+		if Input.is_action_just_pressed("moveDown"):
+			moveDirection = down
+		if Input.is_action_just_pressed("moveLeft"):
+			moveDirection = left
+		if Input.is_action_just_pressed("moveRight"):
+			moveDirection = right
+		if moveDirection:
+			if board.tick.paused:
+				board.resumeTicks()
+
+func spawnChaser(spawnLocation: Vector2) -> void:
+	killActiveTween()
+	position = spawnLocation
+	previousPositions.append(position)
+	var real: Vector2 = gridToReal(position)
+	visual_position.position = real
+	real_position.position = real
+
+func setChaserPositionToStart() -> void:
+	killActiveTween()
+	position = previousPositions[0]
+	var real: Vector2 = gridToReal(position)
+	visual_position.position = real
+	real_position.position = real
+	
 
 # Called on game tick and moves the chaser in the moveDirection
 func updateChaserPosition() -> void:
+	if active:
+		activeChaserMovement()
+	else:
+		oldChaserMovement()
+
+func activeChaserMovement() -> void:
 	killActiveTween()
 	
 	faceDirection()
@@ -45,15 +73,34 @@ func updateChaserPosition() -> void:
 	# Update position with moveDirection and get the real position from board
 	position = position + moveDirection
 	previousPositions.append(position)
-	print(position)
-	var real: Vector2 = gridToReal(position)
+	animateMovementToPosition()
+
 	
+
+func oldChaserMovement() -> void:
+	killActiveTween()
+	
+	faceDirection()
+	
+	# Since I want to allow forr later spawning in, adjust the board tick to a relative tick
+	var relativeTick: int = board.tickCount - startingTick
+	if relativeTick >= len(previousPositions) or relativeTick < 0:
+		return
+
+	position = previousPositions[relativeTick]
+	animateMovementToPosition()
+	
+	
+
+func animateMovementToPosition() -> void:
+	var real: Vector2 = gridToReal(position)
 	currentTween = create_tween()
 	currentTween.tween_property(visual_position, "position", real, tweenTime)
 	real_position.position = real
 
+
 func gridToReal(gridPosition: Vector2) -> Vector2:
-	return board.gridToReal(position) + Vector2(16,16)
+	return board.gridToReal(gridPosition) + Vector2(16,16)
 
 # Rotate visualPosition to be facing the move direction
 func faceDirection() -> void:
@@ -73,3 +120,7 @@ func faceDirection() -> void:
 func killActiveTween() -> void:
 	if currentTween != null and currentTween.is_running():
 		currentTween.kill()
+
+func unactivateChaser() -> void:
+	active = false
+	visual_position.self_modulate = Color.DARK_SLATE_GRAY
